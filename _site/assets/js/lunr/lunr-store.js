9371da45a6a1db271eb8ee9d97490b3073dc0af1
devgaram <joingaram@gmail.com>
2019-01-02 17:50:46 +0900

    티스토리 포스팅 이동 - npm 완료

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,16 @@
 var store = [{
         "title": "npm 시작하기",
         "excerpt":"1. npm이란? npm은 어떻게 구성되어있을까? npm은 세 개의 컴포넌트로 구성되어있습니다. the website - 패키지 탐색, 프로파일 설정, 개인 npm histoty 관리 CLI ( the Command Line Interface ) - 개발자와 npm이 소통하는 방법으로 터미널에서 동작 the registry - 자바스크립트 소프트웨어의 공용 데이터베이스 및 메타 정보 npm은 언제 사용할까? 앱에 패키지를...","categories": [],
-        "tags": [],
+        "tags": ["npm"],
         "url": "http://localhost:4000/npmstart1/",
+        "teaser":"http://localhost:4000/true"},{
+        "title": "npm 로컬 패키지 설치하기",
+        "excerpt":"4. 로컬에 패키지를 설치해보자 패키지를 어떤 방식으로 사용하는지에 따라 우리는 로컬 또는 전역에 npm 패키지를 설치할 수 있습니다. Node.js의 require을 사용하여 당신의 모듈에서 패키지를 사용하려면 로컬로 설치하면 됩니다. 이는 npm 설치의 기본 동작입니다. 패키지를 CLI와 같은 커맨드 라인 툴에서 사용하려면 전역으로 설치하면 됩니다. 패키지 다운로드하기 &gt; npm install &lt;package_name&gt; 명령어를...","categories": [],
+        "tags": ["npm"],
+        "url": "http://localhost:4000/npmstart2/",
+        "teaser":"http://localhost:4000/true"},{
+        "title": "npm Working with package.json",
+        "excerpt":"5. Working with package.json 로컬에 설치된 npm 패키지를 관리하는 최상의 방법은 package.json 파일을 생성하는 것입니다. package.json 파일은 : 프로젝트가 의존하고 있는 패키지들의 리스트를 보여줍니다. semantic versioning rule에 따라 프로젝트에 의존한 패키지들의 버전을 설정할 수 있습니다. 빌드를 재현할 수 있으므로, 좀 더 쉽게 다른 개발자와 공유할 수 있습니다. 요구사항 package.json 파일이...","categories": [],
+        "tags": ["npm"],
+        "url": "http://localhost:4000/npmstart3/",
         "teaser":"http://localhost:4000/true"}]
